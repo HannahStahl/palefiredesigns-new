d7771a30ba8f7e1e2e17d91c7986858f3612dfcf
Hannah Stahl <hannah@oleaedge.com>
2020-04-20 09:23:47 -0500

    Updated footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,13 @@ const Footer = () => (
     <p>
       {`© ${config.businessName}, ${(new Date()).getFullYear()}. All Rights Reserved.`}
     </p>
-    <a href="https://websitesbyhannah.com" target="_blank" rel="noopener noreferrer">
-      <p>Websites By Hannah</p>
-    </a>
+    <p>
+      Website crafted by
+      {' '}
+      <a href="https://websitesbyhannah.com" target="_blank" rel="noopener noreferrer">
+        my daughter Hannah
+      </a>
+    </p>
   </div>
 );
 
