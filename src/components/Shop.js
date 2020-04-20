40ece797e57082cbd8e33e60bf67da1b216c0ad2
Hannah Stahl <hannah@oleaedge.com>
2020-04-20 12:29:54 -0500

    Styled Shop page with Masonry layout

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-component';
 import config from '../config';
 
-const Shop = ({ items }) => (
-  <div>
-    <h1>SHOP</h1>
-    <div className="items">
-      {items.map((item) => (
-        <a
-          key={item.itemId}
-          href={escape(`/items/${item.itemName.replace(/ /g, '_').toLowerCase()}`)}
-          className="item"
-        >
-          <img
-            src={`${config.cloudfrontURL}/${item.itemPhotos[0].photoName}`}
-            alt={item.itemName}
-            className="item-img"
-          />
-          <h3>{item.itemName}</h3>
-        </a>
-      ))}
+const Shop = ({ items }) => {
+  const [etsyItems, setEtsyItems] = useState([]);
+
+  return (
+    <div>
+      <h1>SHOP</h1>
+      <div className="items">
+        <Masonry className="masonry-layout" options={{ isFitWidth: true }}>
+          {items.map((item) => (
+            <a
+              key={item.itemId}
+              href={escape(`/items/${item.itemName.replace(/ /g, '_').toLowerCase()}`)}
+              className="item"
+            >
+              <img
+                src={`${config.cloudfrontURL}/${item.itemPhotos[0].photoName}`}
+                alt={item.itemName}
+                className="item-img"
+              />
+            </a>
+          ))}
+        </Masonry>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Shop;
