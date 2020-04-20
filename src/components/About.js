7031699436db4599387c44c0dcc2732d4f5c553f
Hannah Stahl <hannah@oleaedge.com>
2020-04-20 10:27:13 -0500

    Added temporary photo to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import content from '../content.json';
+import config from '../config';
 
 const About = () => (
   <div>
-    <h1>About</h1>
+    <h1>Meet the Artist: Dale Feuer</h1>
     <div className="bio">
+      <img src={`${config.publicCloudfrontURL}/palefiredesigns-bio.jpg`} alt="Dale Feuer" />
       {content.bio.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
     </div>
   </div>
