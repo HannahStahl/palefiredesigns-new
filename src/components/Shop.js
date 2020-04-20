50bb48ec3a2f6f0eefcf3dd36553afeb455e42b5
Hannah Stahl <hannah@oleaedge.com>
2020-04-20 12:53:13 -0500

    Added addToBag function to Items page in preparation for later

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Masonry from 'react-masonry-component';
 
-const Shop = () => {
+const Shop = ({ updateBag }) => {
   const [etsyItems, setEtsyItems] = useState([]);
 
   useEffect(() => {
     fetch('https://nbqclvrlaf.execute-api.us-east-1.amazonaws.com/dev/listings/get')
       .then((res) => res.json()).then((json) => { setEtsyItems(json.results); });
   }, []);
+
+  const addToBag = (id) => {
+    let bag = JSON.parse(localStorage.getItem('bag'));
+    const newBagItem = { itemId: id, quantity: 1 };
+    if (bag) {
+      const index = bag.findIndex((itemInList) => itemInList.itemId === newBagItem.itemId);
+      const currentBagItem = bag[index];
+      if (currentBagItem) {
+        const newQuantity = currentBagItem.quantity + parseInt(newBagItem.quantity);
+        bag[index].quantity = newQuantity;
+      } else {
+        bag.push(newBagItem);
+      }
+    } else {
+      bag = [newBagItem];
+    }
+    localStorage.setItem('bag', JSON.stringify(bag));
+    updateBag();
+  };
 
   return (
     <div>
