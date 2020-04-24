7f470387353ad15a54bb76b2be515e6bbab935b4
Hannah Stahl <hannah@oleaedge.com>
2020-04-23 21:10:25 -0500

    Horizontally centered quickview

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -76,6 +76,7 @@ const Shop = ({ items, bag, updateBag }) => {
 
   const units = selected !== undefined && getDimensions(items[selected].item_dimensions_unit);
   const quickviewWidth = Math.min(907, 0.8 * window.innerWidth);
+  const quickviewMargin = (window.innerWidth - quickviewWidth) / 2;
 
   console.log(items[selected]);
 
@@ -96,91 +97,95 @@ const Shop = ({ items, bag, updateBag }) => {
           33.3% {
             top: 0;
             left: 0;
-            margin: 10vh 10vw 10vh 10vw;
+            margin: 10vh ${quickviewMargin}px 10vh ${quickviewMargin}px;
             max-height: ${imgHeight + 30}px;
             width: 332px;
           }
           66.7% {
             top: 0;
             left: 0;
-            margin: 10vh 10vw 10vh 10vw;
+            margin: 10vh ${quickviewMargin}px 10vh ${quickviewMargin}px;
             max-height: ${imgHeight + 30}px;
             width: ${quickviewWidth}px;
           }
           100% {
             top: 0;
             left: 0;
-            margin: 10vh 10vw 10vh 10vw;
+            margin: 10vh ${quickviewMargin}px 10vh ${quickviewMargin}px;
             max-height: 80vh;
             width: ${quickviewWidth}px;
           }
+        }
+        .quickview.expanded {
+          margin: 10vh ${quickviewMargin}px 10vh ${quickviewMargin}px;
         }`}
       </style>
       <h1>ALL ITEMS</h1>
       <div className="items">
-        <div className="background-overlay" id="background-overlay" />
-        <div className="quickview" id="quickview">
-          <div className="quickview-img-container" id="quickview-img-container">
-            {selected === undefined ? <></> : (
-              <Carousel activeIndex={imgIndex} onSelect={setImgIndex} interval={false}>
-                {items[selected].Images.map((image) => (
-                  <Carousel.Item key={image.listing_image_id}>
-                    <img
-                      src={selected === undefined ? undefined : image.url_fullxfull}
-                      alt={selected === undefined ? '' : items[selected].title}
-                      className="quickview-img"
-                    />
-                  </Carousel.Item>
-                ))}
-              </Carousel>
-            )}
-          </div>
-          <div className="thumbnails-container">
-            <Masonry className="masonry-layout thumbnails" id="thumbnails" options={{ isFitWidth: true }}>
-              {selected === undefined ? <></> : items[selected].Images.map((image, index) => (
-                <div key={image.listing_image_id} className="thumbnail">
-                  <img
-                    src={image.url_fullxfull}
-                    alt={items[selected].title}
-                    className="thumbnail-img"
-                    id={`thumbnail-${index}`}
-                    onClick={() => setImgIndex(index)}
-                  />
-                </div>
-              ))}
-            </Masonry>
-          </div>
-          <div className="item-details-container">
-            <div className="item-details" id="quickview-details">
+        <div className="background-overlay" id="background-overlay">
+          <div className="quickview" id="quickview">
+            <div className="quickview-img-container" id="quickview-img-container">
               {selected === undefined ? <></> : (
-                <>
-                  <div className="item-price-container">
-                    <h3>{`$${items[selected].price}`}</h3>
-                    <Button size="lg" variant="outline-dark" onClick={addOrRemoveFromBag}>
-                      {getButtonText()}
-                    </Button>
-                  </div>
-                  {(items[selected].item_length || items[selected].item_width) && (
-                    <div className="item-dimensions">
-                      <img src="/dimensions.svg" alt="Dimensions" className="item-details-icon" />
-                      <p>
-                        {items[selected].item_length ? `${items[selected].item_length}${units} long` : ''}
-                        {(items[selected].item_length && items[selected].item_width) ? ', ' : ''}
-                        {items[selected].item_width ? `${items[selected].item_width}${units} wide` : ''}
-                      </p>
-                    </div>
-                  )}
-                  {(items[selected].materials && items[selected].materials.length > 0) && (
-                    <div className="item-materials">
-                      <img src="/materials.svg" alt="Materials" className="item-details-icon" />
-                      <p>{items[selected].materials.map((material, index) => `${index > 0 ? ', ' : ''}${material}`)}</p>
-                    </div>
-                  )}
-                </>
+                <Carousel activeIndex={imgIndex} onSelect={setImgIndex} interval={false}>
+                  {items[selected].Images.map((image) => (
+                    <Carousel.Item key={image.listing_image_id}>
+                      <img
+                        src={selected === undefined ? undefined : image.url_fullxfull}
+                        alt={selected === undefined ? '' : items[selected].title}
+                        className="quickview-img"
+                      />
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
               )}
             </div>
+            <div className="thumbnails-container">
+              <Masonry className="masonry-layout thumbnails" id="thumbnails" options={{ isFitWidth: true }}>
+                {selected === undefined ? <></> : items[selected].Images.map((image, index) => (
+                  <div key={image.listing_image_id} className="thumbnail">
+                    <img
+                      src={image.url_fullxfull}
+                      alt={items[selected].title}
+                      className="thumbnail-img"
+                      id={`thumbnail-${index}`}
+                      onClick={() => setImgIndex(index)}
+                    />
+                  </div>
+                ))}
+              </Masonry>
+            </div>
+            <div className="item-details-container">
+              <div className="item-details" id="quickview-details">
+                {selected === undefined ? <></> : (
+                  <>
+                    <div className="item-price-container">
+                      <h3>{`$${items[selected].price}`}</h3>
+                      <Button size="lg" variant="outline-dark" onClick={addOrRemoveFromBag}>
+                        {getButtonText()}
+                      </Button>
+                    </div>
+                    {(items[selected].item_length || items[selected].item_width) && (
+                      <div className="item-dimensions">
+                        <img src="/dimensions.svg" alt="Dimensions" className="item-details-icon" />
+                        <p>
+                          {items[selected].item_length ? `${items[selected].item_length}${units} long` : ''}
+                          {(items[selected].item_length && items[selected].item_width) ? ', ' : ''}
+                          {items[selected].item_width ? `${items[selected].item_width}${units} wide` : ''}
+                        </p>
+                      </div>
+                    )}
+                    {(items[selected].materials && items[selected].materials.length > 0) && (
+                      <div className="item-materials">
+                        <img src="/materials.svg" alt="Materials" className="item-details-icon" />
+                        <p>{items[selected].materials.map((material, index) => `${index > 0 ? ', ' : ''}${material}`)}</p>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="exit-quickview" onClick={exitQuickview}>x</div>
           </div>
-          <div className="exit-quickview" onClick={exitQuickview}>x</div>
         </div>
         <Masonry
           className="masonry-layout items-list"
