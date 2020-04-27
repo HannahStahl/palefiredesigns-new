80c1b8fea66b44a22610bb2e3d40b2a830314ac0
Hannah Stahl <hannah@oleaedge.com>
2020-04-27 09:30:12 -0500

    Added categories dropdown to navbar SHOP link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import ShoppingBagIcon from './ShoppingBagIcon';
 
 const NavBar = ({ bag }) => (
@@ -9,7 +10,12 @@ const NavBar = ({ bag }) => (
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="ml-auto" activeKey={window.location.pathname}>
-        <Nav.Link href="/items">SHOP</Nav.Link>
+        <NavDropdown title="SHOP" className={window.location.pathname.includes('items') ? 'active' : ''}>
+          <NavDropdown.Item href="/items">ALL</NavDropdown.Item>
+          <NavDropdown.Item href="/items/necklaces">NECKLACES</NavDropdown.Item>
+          <NavDropdown.Item href="/items/bracelets">BRACELETS</NavDropdown.Item>
+          <NavDropdown.Item href="/items/earrings">EARRINGS</NavDropdown.Item>
+        </NavDropdown>
         <Nav.Link href="/about">ABOUT</Nav.Link>
         <Nav.Link href="/contact">CONTACT</Nav.Link>
         <Nav.Link href="/checkout"><ShoppingBagIcon bag={bag} /></Nav.Link>
