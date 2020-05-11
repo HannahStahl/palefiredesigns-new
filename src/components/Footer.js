c2ba8b7f3d76eb7c0d808cb5db76a3e9c8446736
Hannah Stahl <hls62@cornell.edu>
2020-05-11 15:52:50 -0500

    Replaced all instances of "Pale Fire Designs" with "Dale Feuer Jewelry"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import config from '../config';
 
 const Footer = () => (
   <div className="footer">
     <p>
-      {`© ${config.businessName}, ${(new Date()).getFullYear()}. All Rights Reserved.`}
+      {`© Dale Feuer, ${(new Date()).getFullYear()}. All Rights Reserved.`}
     </p>
     <a href="https://websitesbyhannah.com" target="_blank" rel="noopener noreferrer">
       <p>Websites By Hannah</p>
--- src/config.js
+++ src/config.js
@@ -27,6 +27,6 @@ export default {
   // Add common config values here
   cloudfrontURL: `${config.cloudfrontBaseURL}/${config.userID}`,
   publicCloudfrontURL: 'https://d17jmxltsx3ffm.cloudfront.net',
-  businessName: 'Pale Fire Designs',
+  businessName: 'Dale Feuer Jewelry',
   ...config,
 };
