1368229c1ce52352dfcc3fa2c6d4e1b9efe90831
Hannah Stahl <hls62@cornell.edu>
2020-05-26 11:36:42 -0500

    Fixed issue with mobile navbar not collapsing after click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,22 +15,51 @@ export default ({ bag }) => (
       <Nav className="ml-auto" activeKey={window.location.pathname}>
         <NavDropdown title="SHOP" className={window.location.pathname.includes('items') ? 'active' : ''}>
           <NavDropdown.Item
+            eventKey="1"
             as={NavLink}
             to="/items"
             isActive={() => window.location.pathname === '/items'}
           >
             ALL
           </NavDropdown.Item>
-          <NavDropdown.Item className="navbar-dropdown-header">BY CATEGORY</NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/items/necklaces" className="navbar-dropdown-category">Necklaces</NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/items/bracelets" className="navbar-dropdown-category">Bracelets</NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/items/earrings" className="navbar-dropdown-category">Earrings</NavDropdown.Item>
-          <NavDropdown.Item className="navbar-dropdown-header">BY COLLECTION</NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/collections/luxe-cuffs" className="navbar-dropdown-category">Luxe Cuffs</NavDropdown.Item>
+          <div className="navbar-dropdown-header">BY CATEGORY</div>
+          <NavDropdown.Item
+            eventKey="2"
+            as={NavLink}
+            to="/items/necklaces"
+            className="navbar-dropdown-category"
+          >
+            Necklaces
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            eventKey="3"
+            as={NavLink}
+            to="/items/bracelets"
+            className="navbar-dropdown-category"
+          >
+            Bracelets
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            eventKey="4"
+            as={NavLink}
+            to="/items/earrings"
+            className="navbar-dropdown-category"
+          >
+            Earrings
+          </NavDropdown.Item>
+          <div className="navbar-dropdown-header">BY COLLECTION</div>
+          <NavDropdown.Item
+            eventKey="5"
+            as={NavLink}
+            to="/collections/luxe-cuffs"
+            className="navbar-dropdown-category"
+          >
+            Luxe Cuffs
+          </NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link as={NavLink} to="/about">ABOUT</Nav.Link>
-        <Nav.Link as={NavLink} to="/contact">CONTACT</Nav.Link>
-        <Nav.Link as={NavLink} to="/checkout"><ShoppingBagIcon bag={bag} /></Nav.Link>
+        <Nav.Link eventKey="6" as={NavLink} to="/about">ABOUT</Nav.Link>
+        <Nav.Link eventKey="7" as={NavLink} to="/contact">CONTACT</Nav.Link>
+        <Nav.Link eventKey="8" as={NavLink} to="/checkout"><ShoppingBagIcon bag={bag} /></Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
