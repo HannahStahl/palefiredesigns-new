1193959c1e35f16e57ff32b933ceb8fa2e848513
Hannah Stahl <hls62@cornell.edu>
2020-06-04 09:47:34 -0500

    Updated homepage carousel not to pause on hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const getImgUrl = (fileName) => `url("${config.publicCloudfrontURL}/dalefeuerjew
 
 const Home = () => (
   <div className="home">
-    <Carousel indicators={false}>
+    <Carousel indicators={false} pauseOnHover={false}>
       <Carousel.Item>
         <div className="home-section left-text slide-1" style={{ backgroundImage: getImgUrl('jewelry') }}>
           <div className="home-section-text">
